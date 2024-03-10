05da1f71bbb7ca45d65ae6082d9450f0837eeae4
Youness <y.ouzzedi@hotmail.com>
2024-03-10 15:09:43 +0000

    Update homepage title

--- app/page.js
+++ app/page.js
@@ -10,21 +10,7 @@ export default function Home() {
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          
         </div>
       </div>
 
